4abff96b96bae5ac292b5ba2c1e57995b6016f64
SoraY677 <ys4080@icloud.com>
2021-01-15 21:55:43 +0900

    [update]言葉の足し引き機能

--- src/app.py
+++ src/app.py
@@ -1,14 +1,21 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+import json
+from gensim.models import KeyedVectors
 
 app = Flask(__name__)
 app.config['JSON_AS_ASCII'] = False
 
+wv = KeyedVectors.load_word2vec_format('./model.vec.pt', binary=True)
 
-@app.route('/')
+
+@app.route('/moderation')
 def index():
+    param_plus = request.args.get('plus')
+    param_minus = request.args.get('minus')
+    result = wv.most_similar(positive=param_plus, negative=param_minus)[0][0]
     return jsonify({
-        "message": "テスト!!"
-    })
+        "result": result}
+    )
 
 
 if __name__ == '__main__':
